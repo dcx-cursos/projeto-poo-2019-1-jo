a60eacdf49419dec498ea552177f37b76b6f10ea
clebsonf <clebson.augusto@dce.ufpb.br>
2019-08-08 16:13:24 -0300

    ATUALIZAÇÃO

--- src/ufpb/banco/Jogador.java
+++ src/ufpb/banco/Jogador.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Jogador {
 	private String nome;
 	private String cor;
-	private static final Scanner input = new Scanner(System.in);
 
 	
 	public Jogador(String nome, String cor) {
@@ -20,24 +19,19 @@ public class Jogador {
 	public String getCor() {
 		return cor;
 	}
-	public void jogada(){
-		System.out.println("A jogada de "+this.nome+"("+this.cor+") começou:");
-		System.out.println("Comandos disponíveis: [jogar][sair]/n Entre com um comando:" );
-		String opcao = input.nextLine();
-		switch (opcao) {
-			case "jogar":
-				//algumacoisa.jogar();break;
-			case "sair":
-				System.out.println("sim/nao");
-				String sair = input.nextLine();
-				if(sair.equals("sim") || sair.equals("s")) {
-					System.out.println("Jogo encerrado.");
-					System.exit(0);
-				}
-				this.jogada();
-				break;
-		}
+	
+	public void jogada() {
+		// to do
 	}
+	
+	public void status() {
+		//to do
+	}
+	
+	public void sair() {
+		//to do
+	}
+	
 	
 	
 }
--- src/ufpb/banco/Jogo.java
+++ src/ufpb/banco/Jogo.java
@@ -2,21 +2,29 @@ package ufpb.banco;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Jogo {
 	private static final Scanner input = new Scanner(System.in);
 	private int numeroDeJogadores;
-	private ListJogadores jogadores;
+	private LinkedList<Jogador> listaJogadores;
+	
+	
+	//private ListJogadores jogadores;
+	
+	
 	
 	public Jogo() {
-		jogadores = new ListJogadores();
+		//jogadores = new ListJogadores();
+		listaJogadores = new LinkedList<Jogador>();
 	}
 	
 	public void iniciarJogo() {
 		nJogadores();
 		criarJogadores();
 		System.out.println("O Banco Imobiliario vai comecar. Aproveite!");
+		partida();
 
 	}
 	
@@ -37,14 +45,39 @@ public class Jogo {
 			String nome = input.nextLine();
 			System.out.println("Digite a cor:");
 			String cor = input.nextLine();			
-			try {
-				this.jogadores.adicionarNoFim(new Jogador(nome,cor));
-				cont += 1;
-			} catch (corJaUsadaException e) {
-				System.out.println(e.getMessage());
-			}
+			this.listaJogadores.add(new Jogador(nome,cor));
+			cont += 1;
 		}
 	}
 	
-
+	private void opcoes(Jogador j){
+		System.out.println("A jogada de "+j.getNome()+"("+j.getCor()+") começou:");
+		System.out.println("Comandos disponíveis: [jogar][sair]/n Entre com um comando:" );
+		String opcao = input.nextLine();
+		switch (opcao) {
+			case "jogar":
+				j.jogada();
+				break;
+			//IMPLEMENTAR ENCERRANDO QAUNDO FICAR APENAS 1 JOGADOR
+			case "sair":
+				
+				System.out.println("sim/nao");
+				String sair = input.nextLine();
+				if(sair.equals("sim") || sair.equals("s")) {
+					System.out.println("Jogo encerrado.");
+					System.exit(0);
+				}
+				opcoes(j);
+				break;
+		}
+	}
+	
+	private void partida() {
+		for(Jogador x: this.listaJogadores) {
+			opcoes(x);
+		}
+		partida();
+	}
+	
+	
 }
